c6141c49a1d5914b2ba87dcb11dc235c82b1ad99
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-04-02 18:31:20 -0600

    New notes

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create an object and an array which we will use in our facebook exercise.
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+var users = {
+  username: "bpehlert",
+  password: "qwer1234",
+};
+
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+var database = [users];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+var newsfeed = [
+  {
+    username: "mattj",
+    timeline: "These pretzels are making me thirsty",
+  },
+  {
+    username: "mitchs",
+    timeline: "Making some skrilla",
+  },
+  {
+    username: "grace",
+    timeline: "Francine's best moment",
+  }
+];
