344075c5f1b2bb64698729db6d2263f7221cc7b2
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-04-18 16:33:13 -0600

    Added advanced control flow

--- gradient-generator/script.js
+++ gradient-generator/script.js
@@ -1,5 +1,4 @@
 var css = document.querySelector("p");
-// css.textContent = body.style.background + ";";
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
