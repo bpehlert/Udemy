eafe66e5a32c188d5c0e8980b7d9fa3097c210b3
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-04-23 15:18:48 -0600

    New ES5 and ES6 learning

---
+++ ES5-ES6/exercise3.js
@@ -0,0 +1,73 @@
+// change everything below to the newer Javascript!
+
+// let + const
+var a = 'test';
+var b = true;
+var c = 789;
+a = test;
+
+let a = 'test';
+const b = true;
+const c = 789;
+a = test;
+
+
+// Destructuring
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+
+
+const { firstName, lastName, age, eyeColor } = person;
+
+
+
+
+// Object properties
+var a = 'test';
+var b = true;
+var c = 789;
+
+var okObj = {
+  a:a,
+  b:b,
+  c:c
+};
+
+const okObj = {a, b, c};
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+
+const newMessage = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
+
+// default arguments
+// default age to 10;
+function isValidAge(age="10") {
+    return age
+}
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+
+let ben = Symbol("This is my first Symbol");
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+const whereAmI = (username, location) => (username && location) ? "I am not lost" : "I am totally lost!";
