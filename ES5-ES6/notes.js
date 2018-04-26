ec93da320cc01dd0c12fb30ba33ea38e8f6768ff
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-04-26 10:24:45 -0600

    New notes

---
+++ ES5-ES6/notes.js
@@ -0,0 +1,18 @@
+// An arrow function changes traditional code as follows:
+
+let input = 2;
+
+function functionName(input) {
+  let output = input*2;
+  return output;
+};
+
+const arrowName = (input) => {
+  return input*2;
+};
+
+// OR
+
+const arrowName = input => input*2
+
+// You don't need the ()
